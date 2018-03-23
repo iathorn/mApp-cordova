a6f977b01ebea4bd14512ae4456704771786a517
iathorn <kkiathorn@gmail.com>
2018-03-23 12:33:13 +0900

    Passing Data

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  color: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.color = navParams.get('data');
+  }
 
+  ionViewDidLoad(){
+    console.log("ionViewDidLoad AboutPage");
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
+import { ContactPage } from '../contact/contact';
 
 @Component({
   selector: 'page-home',
@@ -8,7 +10,15 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+    
+  }
 
+  goTo(color) {
+    color = color || 'No Color Entered';
+
+    this.navCtrl.push(ContactPage, {
+      data: color
+    });
   }
 
 }
